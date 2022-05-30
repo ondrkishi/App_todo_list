92cdada7d518b98d3d61fceb3a43cd8e24d4f240
ondrkishi <93812971+ondrkishi@users.noreply.github.com>
2022-05-30 09:01:19 +0900

    Add files via upload

--- todo_list/lib/todo_list_state.dart
+++ todo_list/lib/todo_list_state.dart
@@ -10,18 +10,25 @@ class ToDoListState extends StateNotifier<List<ToDoRecord>> {
   Future<void> find() async {
     state = await dbHelper.find();
   }
-  Future<void> add(ToDo todo) async{
-    //登録後の新しいレコードを取得
+
+  Future<void> add(ToDo todo) async {
+    // 登録後の新しいレコードを取得します
     final record = await dbHelper.add(todo);
     state = state.sublist(0)..insert(0, record);
   }
 
+  Future<void> update(ToDoRecord record) async {
+    await dbHelper.update(record.key, record.value);
+    _replaceRecord(record);
+  }
+
   Future<void> toggle(ToDoRecord record) async {
-    //現在のaechivedを反転する
+    // 現在のarchivedを反転します
     final updateRecord = record.copyWith.value(
       archived: !record.value.archived,
     );
 
+    // ToDoを更新します
     await dbHelper.update(
       updateRecord.key,
       updateRecord.value,
@@ -30,20 +37,18 @@ class ToDoListState extends StateNotifier<List<ToDoRecord>> {
     _replaceRecord(updateRecord);
   }
 
-  void _replaceRecord(ToDoRecord record){
-    //配列の同じToDoを入れ替える
+  void _replaceRecord(ToDoRecord record) {
+    // 配列の同じToDoを入れ替えます
     final findIndex = state.indexWhere(
       (e) => e.key == record.key,
-      );
+    );
 
-      //新しいリストで更新する
-      state = List.from(state)
+    // 新しいリストで更新します
+    state = List.from(state)
       ..replaceRange(
         findIndex,
         findIndex + 1,
         [record],
-        );
+      );
   }
-
-  update(ToDoRecord toDoRecord) {}
 }
